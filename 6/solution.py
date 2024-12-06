84d4889464762c1f1cbb3f95205faf709f6f618e
Jacob Hilty <jacobahilty@gmail.com>
2024-12-06 13:12:22 -0500

    Increased speed significantly

--- 6/solution.py
+++ 6/solution.py
@@ -1,4 +1,5 @@
-import copy
+import copy, time
+from collections import defaultdict
 
 starting_position = []
 max_x = 0
@@ -29,6 +30,7 @@ def navigate(map):
   direction = ["N", "E", "S", "W"]
   direction_index = 0
   current_position = starting_position
+  path = defaultdict(list)
   while on_map(current_position) and not_visited(map[str(current_position)], direction[direction_index]):
     candidate_position = current_position
     #Find the next spot
@@ -37,8 +39,8 @@ def navigate(map):
       if (on_map(candidate_position) and "#" in map[str(candidate_position)]) or candidate_position == current_position:
         candidate_position = current_position
         direction_index = (direction_index + 1) % 4
-    if not_visited(map[str(current_position)], direction[direction_index]):
-      map[str(current_position)].append(direction[direction_index])
+    if not_visited(path[str(current_position)], direction[direction_index]):
+      path[str(current_position)].append(direction[direction_index])
     else:
       break
     current_position = candidate_position
@@ -46,7 +48,7 @@ def navigate(map):
     global loops_created
     loops_created += 1
     print("Loop Detected: ", loops_created)
-  return map
+  return path
 
 def on_map(position):
   if position[0] >= 0 and position[0] < max_x and position[1] >= 0 and position[1] < max_y:
@@ -82,20 +84,27 @@ def not_visited(value, direction):
     return False
   return True
 
-def try_all_obstructions(map):
+def visited(value):
+  if len(value) > 0:
+    return True
+  return False
+
+def try_all_obstructions(map, path):
+  count = 0
   for x in range(max_x):
     for y in range(max_y):
-      if map[str([x,y])] != "^" and map[str([x,y])] != "#":
-        new_map = copy.deepcopy(map)
-        new_map[str([x,y])] = ["#"]
-        navigate(new_map)
+      if map[str([x,y])] != "^" and map[str([x,y])] != "#" and visited(path[str([x,y])]):
+        count+=1
+        print(count)
+        map[str([x,y])] = ["#"]
+        navigate(map)
+        map[str([x,y])] = ["."]
 
 def main():
-  initial_map = build_map_dict(import_data())
-  solved_map = navigate(copy.deepcopy(initial_map))
-  print(initial_map == solved_map)
-  print("Part 1:", count_visited(solved_map))
-  try_all_obstructions(initial_map)
+  map = build_map_dict(import_data())
+  path = navigate(map)
+  print("Part 1:", len(path.keys()))
+  try_all_obstructions(map, path)
   print("Part 2:", loops_created)
   
 
